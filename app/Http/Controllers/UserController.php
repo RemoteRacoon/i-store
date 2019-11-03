324762a5b26231707fa38e0f744b1932671723d7
Vladimir199819998 <litvintsev.vova@mail.ru>
2019-11-03 23:55:44 +0800

    Created. Index and show methods added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        $users = User::where('role', 'user')->paginate(5);
+        return response()->json($users);
+    }
+
+    public function show(User $user)
+    {
+        $orders = Order::where('user_id', $user->id)->with(['state', 'product'])->paginate(10);
+        return response()->json(compact('user', 'orders'), 200);
+    }
+
+}
