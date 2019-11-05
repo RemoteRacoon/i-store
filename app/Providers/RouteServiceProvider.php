41107b167b953b14a125dc02738225b175cb3c87
Vladimir <remote_racoon@mail.ru>
2019-11-05 20:50:14 +0800

    Front-end app submodule added

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,9 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
+    /**ts
      * This namespace is applied to your controller routes.
-     *
+     *:
      * In addition, it is set as the URL generator's root namespace.
      *
      * @var string
