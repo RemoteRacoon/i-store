cd3dfde1762a5bc9e4ee2c47266a4cd1bb673b0b
Vladimir <remote_racoon@mail.ru>
2019-11-06 10:58:20 +0800

    index method created

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -15,7 +15,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return response()->json($products, 200);
     }
 
     /**
