be5755f4b938274c411fd579d649492ddab7dbe7
Vladimir <remote_racoon@mail.ru>
2019-11-11 13:45:19 +0800

    AuthService alias added

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
+        'AuthService' => App\Facades\AuthService::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
         'Broadcast' => Illuminate\Support\Facades\Broadcast::class,
         'Bus' => Illuminate\Support\Facades\Bus::class,
