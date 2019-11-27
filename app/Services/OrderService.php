a645ef8829ad44a998af1153500cd6ee4a5b5a64
Vladimir <remote_racoon@mail.ru>
2019-11-27 15:45:15 +0800

    OrderService created

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -4,7 +4,16 @@
 namespace App\Services;
 
 
+use App\Events\OrderConfirmedEvent;
+use App\Order;
+use App\State;
+use App\User;
+
 class OrderService
 {
-
+    public function confirm(Order $confirmed, User $user)
+    {
+        event(new OrderConfirmedEvent($confirmed, $user));
+        return response()->json($confirmed, 200);
+    }
 }
