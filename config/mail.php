6a08f5c0ecbc4c5eb5d0ef23f2b085c81659daa3
Vladimir <remote_racoon@mail.ru>
2019-11-27 15:46:56 +0800

    mail.php config file created

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'i20@mail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Administrator'),
     ],
 
     /*
@@ -116,7 +116,7 @@ return [
         'theme' => 'default',
 
         'paths' => [
-            resource_path('views/vendor/mail'),
+            resource_path('views'),
         ],
     ],
 
